b7a5c33d810dd122faf5449cc217a215dafb6b45
ktipps <62031664+ktipps@users.noreply.github.com>
2021-10-23 08:51:31 -0400

    Connecting JS and running test changes

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,10 +1,14 @@
-(function() {
+
+//Product Details Page Dropbox Selections
+//Citation: Javascript & JQuery Interactive Front End Development, Jon Duckett, pgs 584 - 587
+
+/*(function() {
   var glazeType = document.getElementById('glazeType'); // Selecting Glaze Type box
   var rollQuantity = document.getElementById('rollQuantity'); //Selecting the Roll Quantity Box
 }
 
-addEvent(glazeType, 'change', function(){
-  if(this.value ==- 'choose') {
+addEvent(glazeType, 'change', function() {
+  if(this.value === 'choose') {
     model.innerHTML = '<option>Please choose a glaze first</option>'; 
     return;
   }
@@ -15,94 +19,10 @@ addEvent(glazeType, 'change', function(){
     options += '<option value="' + key + '">' + rollQuantity[key] + '</option>'; 
   }
   model.innerHTML = options;
-});
+});*/
 
+// Changing the Cart Value
 
-
-
-
-/*//add new key=>value to the HTML5 storage
-function SaveItem() {
-			
-	var name = document.forms.ShoppingList.name.value;
-	var data = document.forms.ShoppingList.data.value;
-	localStorage.setItem(name, data);
-	doShowAll();
-	
+function upCounter() {
+	document.getElementById("testing").innerHTML = "2";
 }
-//------------------------------------------------------------------------------
-//change an existing key=>value in the HTML5 storage
-function ModifyItem() {
-	var name1 = document.forms.ShoppingList.name.value;
-	var data1 = document.forms.ShoppingList.data.value;
-	//check if name1 is already exists
-	
-//check if key exists
-			if (localStorage.getItem(name1) !=null)
-			{
-			  //update
-			  localStorage.setItem(name1,data1);
-			  document.forms.ShoppingList.data.value = localStorage.getItem(name1);
-			}
-		
-	
-	doShowAll();
-}
-//-------------------------------------------------------------------------
-//delete an existing key=>value from the HTML5 storage
-function RemoveItem() {
-	var name = document.forms.ShoppingList.name.value;
-	document.forms.ShoppingList.data.value = localStorage.removeItem(name);
-	doShowAll();
-}
-//-------------------------------------------------------------------------------------
-//restart the local storage
-function ClearAll() {
-	localStorage.clear();
-	doShowAll();
-}
-//--------------------------------------------------------------------------------------
-// dynamically populate the table with shopping list items
-//below step can be done via PHP and AJAX too. 
-function doShowAll() {
-	if (CheckBrowser()) {
-		var key = "";
-		var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-		var i = 0;
-		//for more advance feature, you can set cap on max items in the cart
-		for (i = 0; i <= localStorage.length-1; i++) {
-			key = localStorage.key(i);
-			list += "<tr><td>" + key + "</td>\n<td>"
-					+ localStorage.getItem(key) + "</td></tr>\n";
-		}
-		//if no item exists in the cart
-		if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-			list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-		}
-		//bind the data to html table
-		//you can use jQuery too....
-		document.getElementById('list').innerHTML = list;
-	} else {
-		alert('Cannot save shopping list as your browser does not support HTML 5');
-	}
-}
-
-/*
-/*
- =====> Checking the browser support
- //this step may not be required as most of modern browsers do support HTML5
- */
- //below function may be redundant
- /*
-function CheckBrowser() {
-	if ('localStorage' in window && window['localStorage'] !== null) {
-		// we can use localStorage object to store data
-		return true;
-	} else {
-			return false;
-	}
-}
-//-------------------------------------------------
-/*
-You can extend this script by inserting data to database or adding payment processing API to shopping cart..
-*/
