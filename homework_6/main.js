66da9ab4af891862f023eab4dbff087fb296219c
ktipps <62031664+ktipps@users.noreply.github.com>
2021-10-23 20:08:40 -0400

    Getting the cart icon counter to be persistent

--- homework_6/main.js
+++ homework_6/main.js
@@ -21,10 +21,20 @@ addEvent(glazeType, 'change', function() {
   model.innerHTML = options;
 });*/
 
-// Changing the Cart Value
+// Updating the page with the new cart information, the function called when the body of the page loads
+//Gets the value of the cart count
+function pageLoad() {
+	if (!localStorage.getItem("cartIconCount")) { //check if this does not exist
+		localStorage.setItem("cartIconCount", "0");
+	}
+	document.getElementById("cartItemCount").innerHTML=localStorage.getItem("cartIconCount");
+}
 
-var countValue = 0;
-function upCounter() {
-	countValue += 1; 
-	document.getElementById("countValue").innerHTML = countValue;
+// Changing the Cart Value
+//Ciitation: https://stackoverflow.com/questions/22402777/html-javascript-button-click-counter;
+//Citation: Clara Cook, Sidra, and Sohail Bagheri talked through pseudo code with me for this.
+function increaseCartItemCount() {
+	let cartIconCount = Number(localStorage.getItem("cartIconCount")); 
+	cartIconCount += 1; 
+	localStorage.setItem("cartIconCount", cartIconCount);
 }
