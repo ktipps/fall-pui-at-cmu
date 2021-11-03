512858d92b883f6d01aeee3bad331c37954c3f59
ktipps <62031664+ktipps@users.noreply.github.com>
2021-11-02 20:58:02 -0400

    Practicing pseudo code for the assignment

--- homework_6/main.js
+++ homework_6/main.js
@@ -25,16 +25,45 @@ addEvent(glazeType, 'change', function() {
 //Gets the value of the cart count
 function pageLoad() {
 	if (!localStorage.getItem("cartIconCount")) { //check if this does not exist in local storage
-		localStorage.setItem("cartIconCount", "0");
+		localStorage.setItem("cartIconCount", "0"); //If it doesn't exist, create a local storage item with a starting value of 0
 	}
-	document.getElementById("cartItemCount").innerHTML=localStorage.getItem("cartIconCount");
+	document.getElementById("cartItemCount").innerHTML=localStorage.getItem("cartIconCount"); //Target this ID, change the HTML to be the local storage item
 }
 
 // Changing the Cart Value
 //Ciitation: https://stackoverflow.com/questions/22402777/html-javascript-button-click-counter;
 //Citation: Clara Cook, Sidra, and Sohail Bagheri talked through pseudo code with me for this.
-function increaseCartItemCount() {
-	let cartIconCount = Number(localStorage.getItem("cartIconCount")); 
-	cartIconCount += 1; 
-	localStorage.setItem("cartIconCount", cartIconCount);
+
+function detailsButtonClick() {
+  increaseCartItemCount(); 
+  capturingUserSelections(); 
 }
+
+function increaseCartItemCount() {
+	let cartIconCount = Number(localStorage.getItem("cartIconCount"));  //initializing a variable for cartIconCount to store //retrieve local storage item and have variable take on that value
+	cartIconCount += 1; // add one number when clicked
+	localStorage.setItem("cartIconCount", cartIconCount); //then set that back into local storage
+}
+
+/*function capturingUserSelections() {
+    let glazeInput = document.getElementById("glazeType").value; // intializing a variable for glaze input, setting that variable to the value of the selection for glaze
+    localStorage.setItem("glazeType", glazeInput.value); //Creating new local storage object - key is glaze type and value is the value of variable meant to capture the user's selection
+    let bunCountInput = document.getElementById("rollQuantity").value; 
+    Number(bunCountInput);
+    localStorage.setItem("rollQuantity", glazeInput.value);
+}
+*/ 
+
+function CartItem(glazeInput, bunCountInput, flavorInput) {
+  this.glazeInput = glazeInput; 
+  this.bunCountInput = bunCountInput; 
+  this. flavorInput = flavorInput; 
+}
+
+/* on click of the button 
+1.) creating new cart item 
+/* 
+1.) Select a glaze -> that selection value is added to the cartItem object
+2.) Select a quantity -> that selection value is added to the cartItem object
+3.) Store that object in local storage 
+
